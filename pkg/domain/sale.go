f71de1d61084ebfaf0adbf40a4181bab7df73ea8
Somphoph <somphop.homprasert@krungthai.com>
2022-08-29 17:30:13 +0700

    Update field in sale domain.

--- pkg/domain/sale.go
+++ pkg/domain/sale.go
@@ -3,24 +3,43 @@ package domain
 import "time"
 
 type Sale struct {
-	SaleNo   string    `json:"sale-no"`
-	SaleDate time.Time `json:"sale-date"`
-	Customer string    `json:"customer"`
-	Product  string    `json:"product"`
-	Qty      int       `json:"qty"`
-	Price    float32   `json:"price"`
-	ReqDate  time.Time `json:"req-date"`
+	SaleId     int64     `json:"sale-id"`
+	SaleNo     string    `json:"sale-no"`
+	SaleDate   time.Time `json:"sale-date"`
+	CustomerId int64     `json:"customer-id"`
+	ProductId  int64     `json:"product-id"`
+	Qty        int       `json:"qty"`
+	Price      float32   `json:"price"`
+	ReqDate    time.Time `json:"req-date"`
 }
 type Invoice struct {
-	InvNo     string    `json:"inv-no"`
-	InvDate   time.Time `json:"inv-date"`
-	Customer  string    `json:"customer"`
-	Product   string    `json:"product"`
-	Qty       int       `json:"qty"`
-	Price     float32   `json:"price"`
-	ReceiveNo string    `json:"receive-no"`
-	SaleNo    string    `json:"sale-no"`
+	InvId      int64     `json:"inv-id"`
+	InvNo      string    `json:"inv-no"`
+	InvDate    time.Time `json:"inv-date"`
+	CustomerId string    `json:"customer-id"`
+	ProductId  string    `json:"product-id"`
+	Qty        int       `json:"qty"`
+	Price      float32   `json:"price"`
+	ReceiveId  int64     `json:"receive-id"`
+	SaleId     int64     `json:"sale-id"`
 }
+
+type Customer struct {
+	CustomerId  int64  `json:"customer-id"`
+	CusName     string `json:"cus-name"`
+	CusFullName string `json:"cus-full-name"`
+	Addr        string `json:"addr"`
+	Addr1       string `json:"addr-1"`
+}
+
+type Product struct {
+	ProductId       int64   `json:"product-id"`
+	ProductName     string  `json:"product-name"`
+	ProductFullName string  `json:"product-full-name"`
+	Cost            float32 `json:"cost"`
+	Price           float32 `json:"price"`
+}
+
 type SaleSvc interface {
 	Get(saleNo string) (*Sale, error)
 	List() ([]*Sale, error)
